c26926843dbcd071b6a61816c2c653315d94bdc5
CollinNL <collinwoerde@gmail.com>
2016-03-27 14:08:05 +0200

    Help option added

---
+++ app/src/main/java/com/example/royadmiraal/planningpoker/HelpActivity.java
@@ -0,0 +1,15 @@
+package com.example.royadmiraal.planningpoker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Collin on 27-3-2016.
+ */
+public class HelpActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
--- app/src/main/java/com/example/royadmiraal/planningpoker/MainActivity.java
+++ app/src/main/java/com/example/royadmiraal/planningpoker/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.royadmiraal.planningpoker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_help:
+
+                Intent intent = new Intent(this, HelpActivity.class);
+                startActivity(intent);
+
+                return true;
+            default:
+                return true;
+        }
     }
 }
