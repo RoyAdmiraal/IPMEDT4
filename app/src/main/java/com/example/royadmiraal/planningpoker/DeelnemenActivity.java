87e6e05e39145dc6bde6a5db6ed532e77812fd7e
Marius Stolp <mariusstolp@hotmail.com>
2016-04-06 15:16:49 +0200

    Wijzigingen Marius

--- app/src/main/java/com/example/royadmiraal/planningpoker/DeelnemenActivity.java
+++ app/src/main/java/com/example/royadmiraal/planningpoker/DeelnemenActivity.java
@@ -1,10 +1,16 @@
 package com.example.royadmiraal.planningpoker;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DeelnemenActivity extends AppCompatActivity {
 
@@ -33,5 +39,20 @@ public class DeelnemenActivity extends AppCompatActivity {
             default:
                 return true;
         }
+    }
+
+    public void doorgaanSpeelveld(View view) {
+
+        EditText invoerNaam = (EditText)findViewById(R.id.invoerNaam);
+        EditText privateKey = (EditText)findViewById(R.id.privateKey);
+
+        Intent intent = new Intent(DeelnemenActivity.this, SpeelveldActivity.class);
+        intent.putExtra("invoerNaam", invoerNaam.getText().toString());
+        intent.putExtra("privateKey", privateKey.getText().toString());
+
+        Log.d("invoerNaam", "naam wordt geregistreerd");
+        Log.d("privateKey", "PIN komt door");
+
+        startActivity(new Intent(DeelnemenActivity.this, SpeelveldActivity.class));
     }
 }
