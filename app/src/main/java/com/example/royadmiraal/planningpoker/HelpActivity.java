6112c3380be18d624602d9aa73f8ecee3953de16
Marius Stolp <mariusstolp@hotmail.com>
2016-04-07 15:36:17 +0200

    help_activity.xml en strings.xml bijgewerkt, geeft nog wel foutmelding (zie comment in HelpActivity class)

--- app/src/main/java/com/example/royadmiraal/planningpoker/HelpActivity.java
+++ app/src/main/java/com/example/royadmiraal/planningpoker/HelpActivity.java
@@ -2,14 +2,34 @@ package com.example.royadmiraal.planningpoker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.MovementMethod;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by Collin on 27-3-2016.
  */
+
 public class HelpActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
     }
-}
+
+    /* GEEFT FOUTMELDING
+    LINK TOT POGING OM TE LATEN WERKEN
+    http://stackoverflow.com/questions/1748977/making-textview-scrollable-in-android
+
+    final TextView textView2 = new TextView(this);
+    textView2.setMovementMethod(new ScrollingMovementMethod());
+    */
+
+    }
+
+
