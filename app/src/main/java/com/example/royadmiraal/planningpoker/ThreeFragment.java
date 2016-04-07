ee569eebbbed9ce8ac45704b37edb9662bcc85c9
collinnl <collinwoerde@gmail.com>
2016-04-07 15:45:47 +0200

    Resultaten scherm gemaakt met tabs werkend(10st)

---
+++ app/src/main/java/com/example/royadmiraal/planningpoker/ThreeFragment.java
@@ -0,0 +1,34 @@
+package com.example.royadmiraal.planningpoker;
+
+/**
+ * Created by Collin on 7-4-2016.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+//import info.androidhive.materialtabs.R;
+
+
+public class ThreeFragment extends Fragment{
+
+    public ThreeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+
+}
