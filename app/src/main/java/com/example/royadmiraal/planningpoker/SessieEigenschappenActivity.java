f45f17c6b9cb3fc96c0b7ddbe92e38e18aa8c1a8
RoyAdmiraal <s1081752@student.hsleiden.nl>
2016-04-13 12:14:07 +0200

    taak toevoegen

--- app/src/main/java/com/example/royadmiraal/planningpoker/SessieEigenschappenActivity.java
+++ app/src/main/java/com/example/royadmiraal/planningpoker/SessieEigenschappenActivity.java
@@ -3,35 +3,67 @@ package com.example.royadmiraal.planningpoker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.royadmiraal.planningpoker.models.Sessie;
+
 
 public class SessieEigenschappenActivity extends AppCompatActivity {
+
+
+    EditText taakNaam1, taakOmschrijving1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sessie_eigenschappen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+
+        taakNaam1 = (EditText) findViewById(R.id.taakNaam1);
+        taakOmschrijving1 = (EditText) findViewById(R.id.omschrijvingTaak1);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(SessieEigenschappenActivity.this, TaakToevoegenActivity.class);
+                startActivity(intent);
+
+
             }
         });
 
+        Intent i = getIntent();
+
+        Bundle b = getIntent().getExtras();
+
+        if (b != null) {
+            String invoerTaakNaamString = i.getStringExtra("taakNaam");
+            String invoerTaakOmschrijvingString = i.getStringExtra("taakOmschrijving");
+
+
+            taakNaam1.setText(invoerTaakNaamString.toString());
+            taakOmschrijving1.setText(invoerTaakOmschrijvingString.toString());
+        }
+
 
     }
 
-    public void doorgaanSpeelveld(View view) {
+    public void doorgaanSpeelveld (View view){
+
         Intent intent = new Intent(SessieEigenschappenActivity.this, SpeelveldActivity.class);
         startActivity(intent);
-    }
 
+    }
 }
+
+
+
+
+
